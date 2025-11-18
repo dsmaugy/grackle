aa0af3049a08cf0a4bc1895e03388053bf599f1f
Darwin Do <darwin78913@gmail.com>
2025-11-18 00:15:16 -0500

    More volume work

--- components/GrackleLevel.tsx
+++ components/GrackleLevel.tsx
@@ -5,16 +5,16 @@ type GrackleBarProps = {
   width: number;
   currentValue: Accessor<number>;
   maxValue: number;
-  isDraggable?: boolean;
   label: Accessor<string>;
+  onDragClick?: (endValuePct: number) => void;
 };
 
 export const GrackleLevel = ({
   width,
   currentValue,
   maxValue,
-  isDraggable = false,
   label,
+  onDragClick,
 }: GrackleBarProps) => {
   const displayAttrs = createComputed(
     [currentValue, label],
@@ -28,48 +28,49 @@ export const GrackleLevel = ({
     <button
       class="grackle-bar-item internet-button"
       css="padding: 0;"
-      $={(self) => {
-        let gestureStartX = 0;
-        const dragGesture = new Gtk.GestureDrag();
+      $={
+        onDragClick !== undefined
+          ? (self) => {
+              let gestureStartX = 0;
+              const dragGesture = new Gtk.GestureDrag();
 
-        dragGesture.connect("drag-update", (_, x, y) => {
-          const dragPct = Math.max(Math.min((gestureStartX + x) / width, 1), 0);
-          print(`Dragged: X=${x}, Y=${y}. PCT: ${dragPct}%`);
-          // setCurrBrightness(dragPct * (maxBrightness.get() ?? 1));
-          // execAsync(["brightnessctl", "set", `${dragPct * 100}%`]).catch((e) =>
-          //   print(e),
-          // );
-        });
-        dragGesture.connect("drag-end", () => {
-          self.remove_controller(dragGesture);
-          self.add_controller(dragGesture);
-        });
-        dragGesture.connect("drag-begin", (_, x) => {
-          gestureStartX = x;
-        });
+              dragGesture.connect("drag-update", (_, x, y) => {
+                const dragPct = Math.max(
+                  Math.min((gestureStartX + x) / width, 1),
+                  0,
+                );
+                print(`Dragged: X=${x}, Y=${y}. PCT: ${dragPct}%`);
+                onDragClick(dragPct);
+              });
+              dragGesture.connect("drag-end", () => {
+                self.remove_controller(dragGesture);
+                self.add_controller(dragGesture);
+              });
+              dragGesture.connect("drag-begin", (_, x) => {
+                gestureStartX = x;
+              });
 
-        const clickGesture = new Gtk.GestureClick();
-        clickGesture.connect("pressed", (_, n_press, x) => {
-          print(`Clicked at ${x}`);
-          const clickPct = Math.max(Math.min(x / width, 1), 0);
+              const clickGesture = new Gtk.GestureClick();
+              clickGesture.connect("pressed", (_, n_press, x) => {
+                print(`Clicked at ${x}`);
+                const clickPct = Math.max(Math.min(x / width, 1), 0);
+                onDragClick(clickPct);
 
-          // setCurrBrightness(clickPct * (maxBrightness.get() ?? 1));
-          // execAsync(["brightnessctl", "set", `${clickPct * 100}%`]).catch((e) =>
-          //   print(e),
-          // );
-          self.remove_controller(clickGesture);
-          self.add_controller(clickGesture);
-        });
+                self.remove_controller(clickGesture);
+                self.add_controller(clickGesture);
+              });
 
-        self.add_controller(clickGesture);
-        self.add_controller(dragGesture);
-      }}
+              self.add_controller(clickGesture);
+              self.add_controller(dragGesture);
+            }
+          : () => null
+      }
     >
       <With value={displayAttrs}>
         {(attrs) => {
           return (
             <levelbar
-              value={attrs.value}
+              value={Math.min(attrs.value, maxValue)}
               minValue={0}
               maxValue={maxValue}
               widthRequest={width}
--- widget/Volume.tsx
+++ widget/Volume.tsx
@@ -1,16 +1,20 @@
-import { createBinding, With } from "ags";
-import { Gtk } from "ags/gtk4";
-import { execAsync } from "ags/process";
+import { createBinding, createComputed } from "ags";
 import AstalWp from "gi://AstalWp?version=0.1";
 import { GrackleLevel } from "../components/GrackleLevel";
 
 export const Volume = () => {
   const buttonWidth = 100;
-  const maxVol = 1.5;
+  const maxVol = 1.0;
   const { defaultSpeaker: speaker } = AstalWp.get_default()!;
 
   const volume = createBinding(speaker, "volume");
-  const label = volume((vol) => `V: ${Math.round(100 * (vol / maxVol))}`);
+  const speakerStatus = createComputed([
+    volume,
+    createBinding(speaker, "mute"),
+  ]);
+  const label = speakerStatus((stat) =>
+    stat[1] ? "V: ̶M̶" : `V: ${Math.round(100 * (stat[0] / maxVol))}`,
+  );
 
   return (
     <GrackleLevel
@@ -18,6 +22,9 @@ export const Volume = () => {
       maxValue={maxVol}
       currentValue={volume}
       label={label}
+      onDragClick={(endValuePct) => {
+        speaker.set_volume(endValuePct * maxVol);
+      }}
     />
   );
 };
